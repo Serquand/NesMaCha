baf2f0e8a9f1f95cf7147b7fbd3b9a1f63f74232
Serquand <esteban.vincent@efrei.net>
2023-01-26 06:25:55 +0100

    Working on Web

---
+++ web/src/components/LoaderFetch.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="loader">
+        <div class="loader-content"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoaderFetch"
+}
+</script>
+
+<style lang="scss">
+.loader {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(20, 20, 20, .7);
+    z-index: 1500;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-content {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        border-top: 1px solid white;
+        border-left: 1px solid white;
+        animation: loading 1s linear infinite;
+    }
+}
+@keyframes loading {
+    to {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- web/src/views/ValidAccount.vue
+++ web/src/views/ValidAccount.vue
@@ -1,11 +1,52 @@
 <template>
-    <h1>Valid account</h1>
+    <main>
+        <section>
+            <h3>Valider votre compte</h3>
+            <form @submit.prevent="submitCode">
+                <input 
+                    type="text"
+                    placeholder="Code de validation"
+                    v-model="code"
+                >
+                <button type="submit">Valider</button>
+            </form>
+            <Loader v-if="loading"/>
+        </section>
+    </main>
 </template>
 
 <script>
-export default {
+import Loader from '@/components/LoaderFetch.vue';
+import { ref } from 'vue';
+import { URL_API } from "../../config.json";
 
+export default {
+    setup() {
+        const code = ref(""), loading = ref(false);
+        return { code, loading };
+    },
+    methods: {
+        async submitCode() {
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    code: this.code,
+                    idUser: this.$route.params.idUser
+                })
+            };
+
+            this.loading = true;
+            const res = await fetch(URL_API + "/user/validAccount", requestOptions);
+            this.loading = false;
+            console.log(res);
+        }
+    },
+    components: { Loader }
 }
 </script>
 
-<style></style>
+<style lang="scss">
+</style>
