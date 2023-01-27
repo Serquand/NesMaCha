da430583f83c1a2717a7e5ec232e32f16788ead6
Serquand <esteban.vincent@efrei.net>
2023-01-27 15:20:15 +0100

    Setup ws

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,12 +2,17 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import cors, { CorsOptions } from "cors";
 
 import userRouter from "./Router/User";
 import setup from "./Models/Setup";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors:  { origin: process.env.URL_FRONT } });
+
 const PORT = process.env.PORT || 5050;
 
 app.use(express.urlencoded({ extended: false }));
@@ -16,8 +21,12 @@ app.use(cors(process.env.URL_FRONT as CorsOptions));
 
 app.use("/user", userRouter);
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     setup();
-    // console.clear();
+    console.clear();
     console.log("We are listening on PORT", PORT);
 });
+
+io.on("connect", (socket) => {
+    console.log("A new user is now connected !");
+})
